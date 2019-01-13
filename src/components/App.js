67f707d851dfefc8b2a0d3c0ea76ea014eaa8f9c
Andrew Baisden <andrewbaisden@gmail.com>
2019-01-13 14:57:41 +0000

    rent and postcode data passed to bond page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            data: ''
+            data: '',
+            rent: '',
+            postcode: ''
         }
     }
     componentDidMount(){
@@ -16,15 +19,31 @@ class App extends Component {
         const response = await axios.get('https://cxynbjn3wf.execute-api.eu-west-2.amazonaws.com/production/config');
         console.log('Response', response);
         console.log('Data', response.data);
-        this.setState({data: response.data})
+        this.setState({data: response.data.fixed_membership_fee_amount});
+
     } catch(err) {
         console.log(err)
     }
+    }
+    onFormSubmit = (e) => {
+        e.preventDefault()
+        console.log('Clicked Form Submit Button');
+        console.log(this.state.rent);
+        console.log(this.state.postcode);
     }
     render() {
         return(
             <React.Fragment>
                 <h1>Flatbond</h1>
+                <div>
+                    <form onSubmit={this.onFormSubmit}>
+                        <label>Rent</label>
+                        <input type="text" placeholder="Rent" value={this.state.rent} onChange={e => this.setState({rent: e.target.value})} />
+                        <label>Postcode</label>
+                        <input type="text" placeholder="Postcode" value={this.state.postcode} onChange={e => this.setState({postcode: e.target.value})} />
+                        <Link to={{ pathname: '/created-flatbond', state: { rent: this.state.rent, postcode: this.state.postcode } }}>Submit</Link>
+                    </form>
+                </div>
             </React.Fragment>
         )
     }
--- src/views/CreatedFlatBond.js
+++ src/views/CreatedFlatBond.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 class CreatedFlatBond extends Component {
+    state = {
+        rent: '',
+        postcode: ''
+    }
+    componentDidMount(){
+        this.getData();
+    }
+    getData = () => {
+        const rent = this.props.location.state.rent;
+        this.setState({rent: rent})
+
+        const postcode = this.props.location.state.postcode;
+        this.setState({postcode: postcode})
+    }
     render(){
         return(
             <React.Fragment>
                 <h1>Created Flatbond</h1>
+                <p>{this.state.rent}</p>
+                <p>{this.state.postcode}</p>
             </React.Fragment>
         )
     }
