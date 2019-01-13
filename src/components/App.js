4b5091e41a13d62f53b438e3691b5f76bca84ad5
Andrew Baisden <andrewbaisden@gmail.com>
2019-01-13 14:05:47 +0000

    app gets data from API

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: ''
+        }
+    }
+    componentDidMount(){
+        this.getFlatbondAPI()
+    }
+   async getFlatbondAPI(){
+    try {
+        const response = await axios.get('https://cxynbjn3wf.execute-api.eu-west-2.amazonaws.com/production/config');
+        console.log('Response', response);
+        console.log('Data', response.data);
+        this.setState({data: response.data})
+    } catch(err) {
+        console.log(err)
+    }
+    }
     render() {
         return(
             <React.Fragment>
-                <h1>Hello World</h1>
+                <h1>Flatbond</h1>
             </React.Fragment>
         )
     }
